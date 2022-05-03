968d9954301226b8ea4ad1c639859407ad3f73e2
carissamero <carissamero@yahoo.com>
2022-05-03 17:10:34 -0500

    quiz update

---
+++ Assets/questions.js
@@ -0,0 +1,41 @@
+let questions = [{
+        numb: 1,
+        question: "What is the cutest dog breed?",
+        answer: "All of the above",
+        options: [
+            "American Bulldog",
+            "English Bulldog",
+            "French Bulldog",
+            "All of the above",
+        ],
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet",
+        ],
+    },
+    {
+        numb: 3,
+        question: "What is the best state to live in?",
+        answer: "Texas",
+        options: ["Texas", "Not Minnesota", "California", "Florida"],
+    },
+    {
+        numb: 4,
+        question: "Who is Americas Football Team?",
+        answer: "Cowboys",
+        options: ["Patriots", "Bengels", "Cowboys", "Vikings"],
+    },
+    {
+        numb: 5,
+        question: "How many hours of sleep should you get per night?",
+        answer: "8",
+        options: ["0", "6", "8", "None of the above"],
+    },
+];
